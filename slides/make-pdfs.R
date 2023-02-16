96b01bbbbf8f079db2291b60d140f5d30d0c81d8
Ryan Safner <ryansafner@gmail.com>
2023-02-16 13:48:44 -0500

    Update 1.8 slides

--- slides/make-pdfs.R
+++ slides/make-pdfs.R
@@ -1,5 +1,5 @@
 # knit Rmd slides to html
-rmarkdown::render("slides/1.7-slides.Rmd")
+rmarkdown::render("slides/1.8-slides.Rmd")
 
 # make-pdfs
 pagedown::chrome_print(input = "https://ios23.classes.ryansafner.com/slides/1.7-slides.html",

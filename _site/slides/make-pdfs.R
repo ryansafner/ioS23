58b22c71200b70c8d8d522d47e6f90d9bfde483d
Ryan Safner <ryansafner@gmail.com>
2023-02-16 08:20:07 -0500

    1.8 practice, 1.8 pdf, fix 1.7 slides

--- _site/slides/make-pdfs.R
+++ _site/slides/make-pdfs.R
@@ -1,16 +1,14 @@
 # knit Rmd slides to html
-rmarkdown::render("slides/1.6-slides.Rmd")
+rmarkdown::render("slides/1.7-slides.Rmd")
 
 # make-pdfs
-pagedown::chrome_print(input = "https://ios23.classes.ryansafner.com/slides/1.6-slides.html",
-                       output = "slides/1.6-slides.pdf",
+pagedown::chrome_print(input = "https://ios23.classes.ryansafner.com/slides/1.7-slides.html",
+                       output = "slides/1.7-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://ios23.classes.ryansafner.com/slides/1.6-slides.html",
-                       output = "slides/1.6-slides.png",
+pagedown::chrome_print(input = "https://ios23.classes.ryansafner.com/slides/1.8-slides.html",
+                       output = "slides/1.8-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
-
-

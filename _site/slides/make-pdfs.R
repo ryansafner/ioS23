6acb2eae812cd38d2d2e52aa87d330d62ffbe15e
Ryan Safner <ryansafner@gmail.com>
2023-03-23 13:38:42 -0400

    4.1 pdf

--- _site/slides/make-pdfs.R
+++ _site/slides/make-pdfs.R
@@ -2,13 +2,13 @@
 rmarkdown::render("slides/3.3-slides.Rmd")
 
 # make-pdfs
-pagedown::chrome_print(input = "https://ios23.classes.ryansafner.com/slides/3.3-slides.html",
-                       output = "slides/3.3-slides.pdf",
+pagedown::chrome_print(input = "https://ios23.classes.ryansafner.com/slides/4.1-slides.html",
+                       output = "slides/4.1-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://ios23.classes.ryansafner.com/slides/3.3-slides.html",
-                       output = "slides/3.3-slides.png",
+pagedown::chrome_print(input = "https://ios23.classes.ryansafner.com/slides/4.1-slides.html",
+                       output = "slides/4.1-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)

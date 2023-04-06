8787ad10f596ddcfc166a76c0504374b3a7188de
Ryan Safner <ryansafner@gmail.com>
2023-04-06 11:08:15 -0400

    4.4

--- _site/slides/make-pdfs.R
+++ _site/slides/make-pdfs.R
@@ -1,5 +1,5 @@
 # knit Rmd slides to html
-rmarkdown::render("slides/4.3-slides.Rmd")
+rmarkdown::render("slides/4.4-slides.Rmd")
 
 # make-pdfs
 pagedown::chrome_print(input = "https://ios23.classes.ryansafner.com/slides/4.3-slides.html",
